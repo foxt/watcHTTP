7622be0094cb1f46fd23160fe50862a1da825381
Leo Nesfield <leo@thelmgn.com>
2019-08-13 00:50:09 +0100

    Initial Commit

---
+++ watcHTTP WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  watcHTTP WatchKit Extension
+//
+//  Created by Leo Nesfield on 13/8/19.
+//  Copyright © 2019 Leo Nesfield. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ watcHTTP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  watcHTTP
+//
+//  Created by Leo Nesfield on 13/8/19.
+//  Copyright © 2019 Leo Nesfield. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
